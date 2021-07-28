1984c7ad2ddf0c70c9d0cc632284d79d4be30e93
ulmc <cyrille.ulmi@nis.ch>
2021-07-28 10:52:59 +0200

    first commit

---
+++ something.cs
@@ -0,0 +1,15 @@
+namespace hello_world_csharp
+{
+    public class SomeClass
+    {
+        public SomeClass()
+        {
+            NewMethod();
+        }
+
+        private static string NewMethod()
+        {
+            return "hello there";
+        }
+    }
+}
